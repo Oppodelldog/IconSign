b0e73e2b6a2fe129cb55af670d2f5f09e550e818
Oppodelldog <odog@netcologne.de>
2024-10-25 23:46:11 +0200

    prepare v0.4.0

--- IconSign/Plugin.cs
+++ IconSign/Plugin.cs
@@ -13,7 +13,7 @@ namespace IconSign
         // ReSharper disable block MemberCanBePrivate.Global
         public const string PluginGuid = "oppodelldog.mod.iconsign";
         public const string PluginName = "IconSign";
-        public const string PluginVersion = "0.3.0";
+        public const string PluginVersion = "0.4.0";
 
 
         private void Awake()
